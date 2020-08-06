08fae80e677cf0c8eee73f465a81e92944eb7958
Tabish Ashfaq <tabishashfaq@gmail.com>
2020-08-06 12:42:24 -0400

    added if __name__ idiom

--- cgpunet_main.py
+++ cgpunet_main.py
@@ -7,10 +7,6 @@ import os
 import sys
 
 
-#from cgp_config, initialize CGP grid representation
-#level_back = cols
-network_info = CgpInfoConvSet(rows=5, cols=30, level_back=30, min_active_num=1, max_active_num=30)
-
 
 def dir_path(path):
     if os.path.isdir(path):
@@ -18,18 +14,22 @@ def dir_path(path):
     else:
         raise NotADirectoryError(path)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-path', type=dir_path)
 
-args = parser.parse_args()
-dir_name = os.path.dirname(args.path)
-
-print('data path: {}'.format(dir_name))
-
-eval_f = CNNEvaluation(gpu_num=4, dataset_path=dir_name, img_format='*.tif', mask_format='*.tif', input_shape=(128,128,1), target_shape=(128,128,1), out_model='cgpunet_drive')
-
-#CGP object contains a population of 4 individuals
-#TEST: lam=1
-cgp = CGP(network_info, eval_f, max_eval=100, pop_size=4, lam=4, imgSize=128, init=False)
-cgp.modified_evolution(mutation_rate=0.1)
+if __name__ == "__main__":
+    #from cgp_config, initialize CGP grid representation
+    #level_back = cols
+    network_info = CgpInfoConvSet(rows=5, cols=30, level_back=30, min_active_num=1, max_active_num=30)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-path', type=dir_path)
+    
+    args = parser.parse_args()
+    dir_name = os.path.dirname(args.path)
+    
+    print('data path: {}'.format(dir_name))
+    eval_f = CNNEvaluation(gpu_num=4, dataset_path=dir_name, img_format='*.tif', mask_format='*.tif', input_shape=(128,128,1), target_shape=(128,128,1), out_model='cgpunet_drive')
+    
+    #CGP object contains a population of pop_size individuals
+    cgp = CGP(network_info, eval_f, max_eval=100, pop_size=100, lam=4, imgSize=128, init=False)
+    cgp.modified_evolution(mutation_rate=0.1)
 
