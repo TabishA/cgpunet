a554f8eb072e45397dde2792b70eeef6c231fbb5
Nivedha Ramesh <nive77@gmail.com>
2021-06-25 15:31:17 -0400

    Merging opt code

--- knn.py
+++ knn.py
@@ -1,20 +1,41 @@
 import numpy as np
 import networkx as nx
 
+
 # Locate the most similar neighbors
 def get_neighbours(pop, ind, num_neighbors, input_size):
-	distances = list()
-	for p in pop:
-		dist = cosine_similarity(ind, p, input_size)
-		distances.append((p, dist))
-	distances.sort(key=lambda tup: tup[1], reverse=True)
-	neighbors = list()
-	for i in range(num_neighbors):
-		neighbors.append(distances[i])
-	return neighbors
+    distances = list()
+    for p in pop:
+        dist = cosine_similarity(ind, p, input_size)
+        distances.append((p, dist))
+    distances.sort(key=lambda tup: tup[1], reverse=True)
+    neighbors = list()
+    for i in range(num_neighbors):
+        neighbors.append(distances[i])
+    return neighbors
 
 
-def dag_2_vec(G, input_size = (128,128)):
+def calculate_distance(pop, ind):
+    distances = list()
+    for p in pop:
+        dist = cosine_similarity(ind, p)
+        distances.append((p, dist))
+    distances.sort(key=lambda tup: tup[1], reverse=True)
+    return distances
+
+
+# Function to check if a neighboring solution is in the local neighborhood of the candidate solution
+def check_local_neighbor(pop, candidate, neighbor):
+    distances = calculate_distance(pop, neighbor)
+    max_similarity = distances[0][1]
+    print("Best similarity is {}".format(max_similarity))
+    for item in distances:
+        if item[0] == candidate:
+            print("Similarity with current model is {}".format(item[1]))
+            return item[1] == max_similarity
+
+
+def dag_2_vec(G, input_size=(128, 128)):
     vec = []
     depth = nx.shortest_path_length(G, 'input_0_0')
     for node in G.nodes():
@@ -22,8 +43,8 @@ def dag_2_vec(G, input_size = (128,128)):
         pf = node_dict['pool_factor']
         num_ch = node_dict['num_channels']
         if pf > 0:
-            x = input_size[0]/(2*pf)
-            y = input_size[1]/(2*pf)
+            x = input_size[0] / (2 * pf)
+            y = input_size[1] / (2 * pf)
         elif pf == 0 and num_ch == 0:
             x = 1
             y = int(node_dict['units'])
@@ -31,8 +52,8 @@ def dag_2_vec(G, input_size = (128,128)):
             x = input_size[0]
             y = input_size[1]
         else:
-            x = input_size[0]*(2*abs(pf))
-            y = input_size[1]*(2*abs(pf))
+            x = input_size[0] * (2 * abs(pf))
+            y = input_size[1] * (2 * abs(pf))
         vec.append([int(node_dict['num_channels']), x, y, depth[node]])
 
     return vec
@@ -42,35 +63,34 @@ def cosine_similarity(G1, G2, input_size):
     v1 = dag_2_vec(G1, input_size)
     v2 = dag_2_vec(G2, input_size)
 
-    v1 = v1[1:len(v1)-1]
-    v2 = v2[1:len(v2)-1]
+    v1 = v1[1:len(v1) - 1]
+    v2 = v2[1:len(v2) - 1]
 
-    #in case of unequal length, make sure v1 is the longer of the two
+    # in case of unequal length, make sure v1 is the longer of the two
     if len(v2) > len(v1):
         temp = v1.copy()
         v1 = v2
         v2 = temp
-    
+
     length_diff = len(v1) - len(v2)
 
     for _ in range(length_diff):
         v2.append([0, 0, 0, 0])
 
-    assert(len(v1) == len(v2))
-    
+    assert (len(v1) == len(v2))
+
     similarity = np.zeros(len(v1))
 
     for i in range(len(v1)):
         if np.linalg.norm(v2[i]) == 0:
             c_sim = 0
         else:
-            c_sim = np.dot(v1[i], v2[i])/(np.linalg.norm(v1[i])*np.linalg.norm(v2[i]))
-        
+            c_sim = np.dot(v1[i], v2[i]) / (np.linalg.norm(v1[i]) * np.linalg.norm(v2[i]))
+
         similarity[i] = c_sim
 
-    
     sum_sim = np.sum(similarity)
 
-    norm_sum = sum_sim/len(v1)
+    norm_sum = sum_sim / len(v1)
 
     return norm_sum
