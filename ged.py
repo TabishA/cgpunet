b16d1387caa90144c2445aeff446e69687a8d448
Nivedha Ramesh <nive77@gmail.com>
2022-06-03 21:47:40 -0400

    Add files via upload

--- ged.py
+++ ged.py
@@ -6,8 +6,9 @@ from networkx.algorithms import similarity
 import concurrent.futures
 import time
 
+
 def match(n1, n2):
-    #return n1['num_channels'] == n2['num_channels'] and n1['pool_factor'] == n2['pool_factor']
+    # return n1['num_channels'] == n2['num_channels'] and n1['pool_factor'] == n2['pool_factor']
     return n1['function'] == n2['function']
 
 
@@ -17,8 +18,8 @@ def ged(args):
     ind1, ind2 = args
     G1 = cgp_2_dag(ind1.active_net_list())
     G2 = cgp_2_dag(ind2.active_net_list())
-    #G1 = ind1
-    #G2 = ind2
+    # G1 = ind1
+    # G2 = ind2
     duration = 0
     start = time.time()
     sim_gp = None
@@ -30,8 +31,7 @@ def ged(args):
                 print('Timeout')
                 break
         break
-    
-    
+
     print('SIM: {}'.format(sim_gp))
 
     return sim_gp, ind2
@@ -39,7 +39,7 @@ def ged(args):
 
 def get_iterable(pop, ind):
     it = []
-    for p in pop: 
+    for p in pop:
         it.append((ind, p))
     return it
 
@@ -48,39 +48,49 @@ def get_iterable(pop, ind):
 def get_neighbours(pop, ind, num_neighbors):
     distances = list()
     args = get_iterable(pop, ind)
-    
+
     with concurrent.futures.ProcessPoolExecutor() as executor:
         result = executor.map(ged, args)
-    
 
     for dist, ind2 in result:
         distances.append((ind2, dist))
-	
-    #distances.sort(key=lambda tup: tup[1], reverse=True)
+
+    # distances.sort(key=lambda tup: tup[1], reverse=True)
     distances.sort(key=lambda tup: tup[1])
     neighbors = list()
     for i in range(num_neighbors):
         neighbors.append(distances[i])
-	
+
     return neighbors
 
 
 def get_distances(pop, ind):
     distances = dict()
     args = get_iterable(pop, ind)
-    
+
     with concurrent.futures.ProcessPoolExecutor() as executor:
         result = executor.map(ged, args)
-    
 
     for dist, ind2 in result:
         distances[ind2.model_name] = dist
-	
-    #distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
-    #distances.sort(key=lambda tup: tup[1])
-	
+
+    # distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
+    # distances.sort(key=lambda tup: tup[1])
+
     return distances
 
 
+def get_distances_simgnn(simgnn_model, data):
+    """
+    data : a list of dictionaries of this form [{g1, g2, l1, l2, m1, m2}]
+
+    return : {individual.modelname: ged}
+    """
+    result = dict()
+    for pair in data:
+        result[pair['modelname2']] = simgnn_model.predict(pair)
+
+# New get distance function to be coded here using the trained model
+# Model inputs are individuals in json format
 def entropy(pop):
     pass
